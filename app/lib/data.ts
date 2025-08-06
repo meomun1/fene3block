4660dd1d4ce450f20c569001aebb60730eb3ad29
meomun1 <maluong1504@gmail.com>
2025-08-06 13:33:34 +0700

    feat: add search and pagination for invoices

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,12 +13,6 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
     // console.log('Data fetch completed after 3 seconds.');
@@ -32,8 +26,6 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -54,8 +46,6 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   try {
-
-    await new Promise((resolve) => setTimeout(resolve, 500));
 
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
