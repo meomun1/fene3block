09186134a2241df5b166658efe47d5a33409d275
meomun1 <maluong1504@gmail.com>
2025-08-07 00:01:01 +0700

    chore: add metadata for fene3block

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import  '@/app/ui/global.css'
 import { inter } from './ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Fene3block',
+  description: 'Fene3block',
+  metadataBase: new URL('https://fene3block.vercel.app/'),
+};
 
 export default function RootLayout({
   children,
